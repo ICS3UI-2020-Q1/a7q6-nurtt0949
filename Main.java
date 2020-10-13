6d5b9c3e594f7a8a947b706b3de569b0224797a0
nurtt0949 <71143068+nurtt0949@users.noreply.github.com>
2020-10-13 17:29:58 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,36 @@
 /**
- *
- * @author 
+ *Odd digits are true even digits are false
+ * @author Tolga Nurtekin
  */
 public class Main {
+
+  public static boolean allDigitsOdd(int integer){
+    //create a loop to run as the integer is greater or less than 0
+    while(integer > 0 || integer < 0){
+      //make digit variable as a remainder 
+      int digit = integer % 10;
+      //divide the integer to find each digit
+      integer /=10;
+
+      //if digits are even return false
+      if(digit % 2 == 0){
+       return false;
+      }
+    
+   } 
+    return true; 
+  }
+   
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //delcare a variable for users final number
+    boolean total = allDigitsOdd(3799);
+    //tell user if true or false
+    System.out.println(total);
     
   }
 }
